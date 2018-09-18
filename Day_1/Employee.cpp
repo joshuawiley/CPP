44507e255d1d16b1bf60e149b8fe74f59f1962dc
joshuawiley <joshuaw1@kpit.com>
2018-09-18 11:11:25 -0400

    Uncommented code

--- Day_1/Employee.cpp
+++ Day_1/Employee.cpp
@@ -2,119 +2,119 @@
 #include <iostream>
 #include <stdlib.h>
 
-//using namespace std;
-//
-//class Employee
-//{
-//private:
-//    int employeeID;
-//    string name;
-//    string location;
-//
-//public:
-//    Employee();
-//    ~Employee();
-//
-//    void getEmployee();
-//    virtual void getEmployee(int id);
-//    virtual void setEmployee(int empID,
-//                             string empName,
-//                             string empLocation);
-//    int operator++()
-//    {
-//        cout << "in prefix increment" << endl;
-//        return employeeID++;
-//    }
-//    Employee& operator++(int a)
-//    {
-//        cout << "in postfix increment" << endl;
-//        employeeID++;
-//        return *this;
-//    }
-//
-//    int operator+(Employee obj)
-//    {
-//        cout << "in add op" << endl;
-//        int ret = this->employeeID + obj.employeeID;
-//        return ret;
-//    }
-//
-//    int operator-(Employee obj)
-//    {
-//        cout << "in add op" << endl;
-//        int ret = this->employeeID - obj.employeeID;
-//        return ret;
-//    }
-//
-//    void printAll();
-//};
-//
-//Employee::Employee()
-//{
-//    employeeID = 0000000;
-//    name = "John Doe";
-//    location = "Novi, MI";
-//}
-//
-//Employee::~Employee()
-//{
-//}
-//
-//void Employee::getEmployee()
-//{
-//    cout << "Employee ID: " << employeeID <<endl;
-//    cout << "Name: " << name <<endl;
-//    cout << "Location: " << location <<endl;
-//}
-//void Employee::getEmployee(int id)
-//{
-//    cout << "Employee ID: " << id <<endl;
-//    cout << "Name: " << name <<endl;
-//    cout << "Location: " << location <<endl;
-//}
-//
-//void Employee::setEmployee(int employeeID,
-//                           string name = "Jane Doe",
-//                           string location = "Alpena, MI")
-//{
-//    this->employeeID = employeeID;
-//    this->name = name;
-//    this->location = location;
-//}
-///*
-//void Employee::setEmployee(int employeeID,
-//                           string name,
-//                           string location)
-//{
-//    this->employeeID = employeeID;
-//    this->name = name;
-//    this->location = location;
-//}
-//*/
-//void Employee::printAll()
-//{
-//    cout << "Employee ID: " << employeeID <<endl;
-//    cout << "Name: " << name <<endl;
-//    cout << "Location: " << location <<endl;
-//}
-//
-//int main(void)
-//{
-//    int ret_val;
-//    Employee emp, emp1;
-//    emp1.setEmployee(111111);
-//    emp.printAll();
-//    emp.setEmployee(222222);
-//    emp.printAll();
-//
-//    emp = emp++;
-//    emp.printAll();
-//
-//    ret_val = emp + emp1;
-//    cout << "ret_val = emp + emp1: " << ret_val << endl;
-//    emp.printAll();
-//
-//    ret_val = emp - emp1;
-//    cout << "ret_val = emp - emp1: " << ret_val << endl;
-//    emp.printAll();
-//}
+using namespace std;
+
+class Employee
+{
+private:
+    int employeeID;
+    string name;
+    string location;
+
+public:
+    Employee();
+    ~Employee();
+
+    void getEmployee();
+    virtual void getEmployee(int id);
+    virtual void setEmployee(int empID,
+                             string empName,
+                             string empLocation);
+    int operator++()
+    {
+        cout << "in prefix increment" << endl;
+        return employeeID++;
+    }
+    Employee& operator++(int a)
+    {
+        cout << "in postfix increment" << endl;
+        employeeID++;
+        return *this;
+    }
+
+    int operator+(Employee obj)
+    {
+        cout << "in add op" << endl;
+        int ret = this->employeeID + obj.employeeID;
+        return ret;
+    }
+
+    int operator-(Employee obj)
+    {
+        cout << "in add op" << endl;
+        int ret = this->employeeID - obj.employeeID;
+        return ret;
+    }
+
+    void printAll();
+};
+
+Employee::Employee()
+{
+    employeeID = 0000000;
+    name = "John Doe";
+    location = "Novi, MI";
+}
+
+Employee::~Employee()
+{
+}
+
+void Employee::getEmployee()
+{
+    cout << "Employee ID: " << employeeID <<endl;
+    cout << "Name: " << name <<endl;
+    cout << "Location: " << location <<endl;
+}
+void Employee::getEmployee(int id)
+{
+    cout << "Employee ID: " << id <<endl;
+    cout << "Name: " << name <<endl;
+    cout << "Location: " << location <<endl;
+}
+
+void Employee::setEmployee(int employeeID,
+                           string name = "Jane Doe",
+                           string location = "Alpena, MI")
+{
+    this->employeeID = employeeID;
+    this->name = name;
+    this->location = location;
+}
+/*
+void Employee::setEmployee(int employeeID,
+                           string name,
+                           string location)
+{
+    this->employeeID = employeeID;
+    this->name = name;
+    this->location = location;
+}
+*/
+void Employee::printAll()
+{
+    cout << "Employee ID: " << employeeID <<endl;
+    cout << "Name: " << name <<endl;
+    cout << "Location: " << location <<endl;
+}
+
+int main(void)
+{
+    int ret_val;
+    Employee emp, emp1;
+    emp1.setEmployee(111111);
+    emp.printAll();
+    emp.setEmployee(222222);
+    emp.printAll();
+
+    emp = emp++;
+    emp.printAll();
+
+    ret_val = emp + emp1;
+    cout << "ret_val = emp + emp1: " << ret_val << endl;
+    emp.printAll();
+
+    ret_val = emp - emp1;
+    cout << "ret_val = emp - emp1: " << ret_val << endl;
+    emp.printAll();
+}
--- Day_1/scope.cpp
+++ Day_1/scope.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
 
-// using namespace std;
+using namespace std;
 
 int i = 80;
-//int main(void)
-//{
-//    int i = 50;
-//    bool a = true;
-//
-//    std::cout << "i = " << i << " global i = " << ::i << std::endl;
-//
-//    if (a == true)
-//    {
-//        int i = 20;
-//        std::cout << "i = " << i << " global i = " << ::i << std::endl;
-//        i++;
-//        ::i++;
-//        std::cout << "i++ = " << i << " global i++ = " << ::i << std::endl;
-//    }
-//    std::cout << "i = " << i << " global i = " << ::i << std::endl;
-//    return 0;
-//}
+int main(void)
+{
+    int i = 50;
+    bool a = true;
+
+    std::cout << "i = " << i << " global i = " << ::i << std::endl;
+
+    if (a == true)
+    {
+        int i = 20;
+        std::cout << "i = " << i << " global i = " << ::i << std::endl;
+        i++;
+        ::i++;
+        std::cout << "i++ = " << i << " global i++ = " << ::i << std::endl;
+    }
+    std::cout << "i = " << i << " global i = " << ::i << std::endl;
+    return 0;
+}
